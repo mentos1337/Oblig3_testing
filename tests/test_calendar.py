3ffbc71905bcaa9ba1081dc8b25bc1ef0aafe5c9
Granit <65214984+mentos1337@users.noreply.github.com>
2023-10-26 12:31:39 +0200

    Update test_calendar.py

--- tests/test_calendar.py
+++ tests/test_calendar.py
@@ -1,20 +1,20 @@
 import pytest
 from Kalender import isLeapYear
-@pytest.mark.test
+
 def test_is_divisible_by_4_but_not_100():
     assert isLeapYear(1240) == True
-@pytest.mark.test
+
 def test_is_divisible_by_400():
     assert isLeapYear(400) == True
-@pytest.mark.test
+
 def test_is_divisible_by_4_or_400():
     assert isLeapYear(2000) == True
-@pytest.mark.test
+
 def test_is_not_divisible_by_4():
     assert isLeapYear(2111) == False
-@pytest.mark.test
+
 def test_is_divisible_by_100_not_div_by_400():
     assert isLeapYear(100) == False
-@pytest.mark.test
+
 def test_is_not_a_leap_year():
     assert isLeapYear(1700) == False
